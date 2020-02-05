cfeb8d330cc18a30232e193b6cb4cb6990c5d5b2
Ashna Shah-Grover <as19@williams.edu>
2020-02-05 11:32:37 -0500

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,30 @@
 // code solution here
+
+process.argv.shift()
+process.argv.shift()
+
+if (process.argv.length!=2){
+  console.log("Wrong Number of Arguments: Enter 2 Arguments")
+} else if (process.argv[0]) {
+
+}
+
+function generatePassword(a, b){
+  let passwords=[]
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let password=""
+  let num=1
+  while (num<=b){
+    num++
+    for (let i=1; i<=a;i++){
+      password+=characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    passwords.push(password)
+    password=""
+  }
+  passwords.forEach(password=>console.log(password))
+}
+
+generatePassword(process.argv[0], process.argv[1])
+
+process.exit(0)
